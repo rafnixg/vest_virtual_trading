c46ce35a00813b83e21f3baff4b9367aecbc5d03
Rafnix Guzmán <rafnixg@gmail.com>
2022-03-24 23:25:20 -0500

    ADD - Database SQLite3 settings

---
+++ db.py
@@ -0,0 +1,24 @@
+"""Database session factory"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    """get_db - Database session factory."""
+    session = SessionLocal()
+    try:
+        yield session
+        session.commit()
+    finally:
+        session.close()
