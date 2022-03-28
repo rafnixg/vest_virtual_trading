796ee3f59851d1aa628d84c368f19b7e93c2c8e8
Rafnix Guzmán <rafnixg@gmail.com>
2022-03-27 19:59:11 -0500

    IMP - Clean blank spaces

--- utils.py
+++ utils.py
@@ -1,10 +1,6 @@
 """utilities"""
 
-import re
-
-
 def extrac_price(data):
     """Extract price from data."""
     price_str = data["primaryData"]["lastSalePrice"][1:]
     return float(price_str)
-
